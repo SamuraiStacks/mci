f3f3dd309446e4cd337f0754da694194919b1e4a
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-02 08:51:17 -0400

    Delete bin/cmds/watermellon.js

--- bin/cmds/watermellon.js
+++
@@ -1,9 +0,0 @@
-exports.run = () => {
-    console.log("Rob > Im going to sneeze!")
-    console.log("Rob > AAAAAAAAAA")
-    console.log("Gary > say watermellon")
-    console.log("Rob > watermellon")
-    setTimeout(() => {
-        console.log("Rob > oof")
-    }, 5000)
-}
