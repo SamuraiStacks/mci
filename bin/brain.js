c0faeee6c7482b342ad3c06723cc20267e4ea6b9
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-02 08:40:12 -0400

    v1

--- bin/brain.js
+++ bin/brain.js
@@ -2,13 +2,13 @@
 let args = process.argv.slice(2)
 
 try {
-    const cmd = require(`./cmds/${args[0]}`)
+    const cmd = require(`./commands/${args[0]}`)
     cmd.run(args)
 } catch(err) {
     if(err.code === "MODULE_NOT_FOUND") {
-        const cmd = require(`./cmds/help.js`)
+        const cmd = require(`./commands/help.js`)
         cmd.run(args)
     } else {
-        console.log(err.message)
+        console.log(`Error: ${err.message}`)
     }
 }
