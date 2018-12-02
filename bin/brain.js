e238432283a259325f335ce5416372bf32643da1
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-02 09:12:01 -0400

    demo

--- bin/brain.js
+++ bin/brain.js
@@ -1,14 +1,21 @@
 #! /usr/bin/env node
 let args = process.argv.slice(2)
 
-try {
-    const cmd = require(`./commands/${args[0]}`)
-    cmd.run(args)
-} catch(err) {
-    if(err.code === "MODULE_NOT_FOUND") {
-        const cmd = require(`./commands/help.js`)
+if(!args[0]) {
+    console.log("Commands:\nping")
+    process.stdin.on("data", (data) => {
+        require(`./commands/${data.slice(0, -2)}.js`).run(args)
+    })
+} else {
+    try {
+        const cmd = require(`./commands/${args[0]}`)
         cmd.run(args)
-    } else {
-        console.log(`Error: ${err.message}`)
+    } catch(err) {
+        if(err.code === "MODULE_NOT_FOUND") {
+            const cmd = require(`./commands/help.js`)
+            cmd.run(args)
+        } else {
+            console.log(`Error: ${err.message}\n${err.stack}`)
+        }
     }
 }
