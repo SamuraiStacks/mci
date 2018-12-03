4650965bfb3f6fe1ce03246a14be9482788c0807
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-02 21:28:42 -0400

    forgot something

--- bin/commands/hack.js
+++ bin/commands/hack.js
@@ -2,7 +2,7 @@ let i = 0;
 
 exports.run = (args, utils) => {
     const hacks = [`Hacking ${args[1]}'s computer...`, "Getting their IP", `IP Address found! ${utils.getIP()}`]
-    if(!args) return console.log("Who to hacc?")
+    if(!args[1]) return console.log("Who to hacc?")
     setTimeout(useHacks, 3000);
     function useHacks() {
         utils.write(hacks[i++])
