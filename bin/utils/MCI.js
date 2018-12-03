4f22fb3bc5efe480af27e382b11641c57581525f
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-02 21:28:10 -0400

    ip grabber + copypasta

--- bin/utils/MCI.js
+++ bin/utils/MCI.js
@@ -1,1 +1,14 @@
-
+module.exports = {
+    write: (str = '') => {
+        process.stdout.cursorTo(0);
+        process.stdout.clearLine(1);
+        process.stdout.write(str);
+    },
+    getIP: () => {
+        let ipparts = []
+        for (let i = 0; i < 4; i++) {
+            ipparts.push(Math.floor(Math.random() * 255))
+        }
+        return ipparts.join(".")
+    }
+}
