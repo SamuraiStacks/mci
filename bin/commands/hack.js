a45e18e8693045f71da4d726ffaaca5f08c3fcf8
SamuraiStackszz <38820196+SamuraiStacks@users.noreply.github.com>
2018-12-14 21:24:24 -0400

    uhhh

--- bin/commands/hack.js
+++ bin/commands/hack.js
@@ -1,7 +1,7 @@
 let i = 0;
 
 exports.run = (args, utils) => {
-    const hacks = [`Hacking ${args[1]}'s computer...`, "Getting their IP", `IP Address found! ${utils.getIP()}`]
+    const hacks = [`Hacking ${args[1]}'s computer...`, "Getting their IP", `IP Address found! ${utils.getIP()}`, "Tracing their location..", "Location found: 69 Coke Street, Meme Town", "Dicking Girls..", "Hot Stuff has come!"]
     if(!args[1]) return console.log("Who to hacc?")
     setTimeout(useHacks, 3000);
     function useHacks() {
